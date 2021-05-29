312b5b43789bd4a08287238faf03335cfd82530c
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2021-05-29 22:50:47 +0600

    queue

---
+++ lab/lab_5/copyQueue.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+
+    int data;
+    struct Node *next;
+};
+
+void copyQueue(struct Node *head, int data_size, int data[]){
+
+
+}
+
+void main(){
+
+
+}
