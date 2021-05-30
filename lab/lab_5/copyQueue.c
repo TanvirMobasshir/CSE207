6bb0b97be17abca7a43bbfd71f8da3b624c1eea2
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2021-05-30 09:13:27 +0600

    copyQueue

--- lab/lab_5/copyQueue.c
+++ lab/lab_5/copyQueue.c
@@ -7,12 +7,67 @@ struct Node{
     struct Node *next;
 };
 
-void copyQueue(struct Node *head, int data_size, int data[]){
+void enqueue(struct Node **front, struct Node **rear, int data){
 
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->data = data;
+    node->next = NULL;
 
+    if (*front == NULL){
+
+        *front = node;
+        *rear = node;
+    }
+    else {
+
+        (*rear)->next = node;
+        *rear = node;
+    }
+}
+
+void display(struct Node *head){
+
+    if (head == NULL){
+
+        printf("The queue is empty.\n\n");
+        return;
+    }
+
+    while (head != NULL){
+    
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void copyQueue(struct Node *head){
+
+    struct Node *cfront = NULL, *crear = NULL;
+
+    while (head != NULL){
+
+        enqueue(&cfront, &crear, head->data);
+        head = head->next;
+    }
+    printf("Q2: ");
+    display(cfront);
 }
 
 void main(){
 
+    struct Node *front = NULL, *rear = NULL;
 
+    int data_size, data;
+    printf("Enter the Number of elements: ");
+    scanf("%d", &data_size);
+
+    for (int i = 0; i < data_size; i++){
+    
+        scanf("%d", &data);
+        enqueue(&front, &rear, data);
+    }
+    printf("Q1: ");
+    display(front);
+    copyQueue(front);
 }
